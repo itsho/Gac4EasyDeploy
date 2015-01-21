8ef2a05fbc3407dec8c97ece756ee876625a1c68
itsho <itsho.itsho@gmail.com>
2015-01-21 15:16:18 +0200

    First Checkin

---
+++ Gac4EasyDeploy/Controller/GacManagerController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Input;
+using Gac4EasyDeploy.Model;
+
+namespace Gac4EasyDeploy.Controller
+{
+    internal class GacManagerController
+    {
+        private readonly GacManagerModel m_gacManagerModel;
+
+
+        //HKEY_LOCAL_MACHINE\Software\Microsoft\Fusion  (DWORD) DisableCacheViewer = 1
+        public ICommand OpenFusionLog;
+
+
+        public GacManagerController(GacManagerModel p_gacManagerModel)
+        {
+            m_gacManagerModel = p_gacManagerModel;
+        }
+
+        public void AddFileToSymbolsStore(String p_strPDBFile)
+        {
+            //symstore add /f <path to PDB> /s C:\symbols /t "My Product"
+        }
+
+
+        public void Deployfile(string p_strAssemblyPath)
+        {
+            foreach (string strExtention in m_gacManagerModel.ExtraExtentions)
+            {
+                string pdbFilename = string.Format("{0}.{1}", Path.GetFileNameWithoutExtension(p_strAssemblyPath), strExtention);
+
+                string pdbPath = Path.Combine(Path.GetDirectoryName(p_strAssemblyPath), pdbFilename);
+
+                //if source PDB is found:
+
+                if (File.Exists(pdbPath))
+                {
+                    //get the strong name info from the assembly:
+
+                    AssemblyName gacAssemblyName = Assembly.LoadFile(p_strAssemblyPath).GetName();
+
+                    string gacPath = GetGacPhysicalLocation(gacAssemblyName);
+
+                    //if GAC location found:
+
+                    if (!string.IsNullOrEmpty(gacPath))
+                    {
+
+                        try
+                        {
+                            File.Copy(pdbPath, Path.Combine(gacPath, pdbFilename));
+
+                        }
+                        catch (Exception ex)
+                        {
+                        }
+                    }
+                }  
+            }
+         
+        }
+
+        private string GetGacPhysicalLocation(AssemblyName gacAssemblyName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Gac4EasyDeploy/Model/GacManagerModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gac4EasyDeploy.Model
+{
+    public class GacManagerModel
+    {
+        public string[] ExtraExtentions
+        {
+            get;
+            set;
+        }
+
+        public GacManagerModel()
+        {
+            ExtraExtentions = new string[3] { "pdb", "xml","config" };
+
+
+        }
+    }
+}
